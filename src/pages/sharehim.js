a80a11f329fb858453c3f50f8580b1afad7e7413
Chris Lunsford <cm@lunsford.io>
2023-02-16 21:23:21 -0500

    WIP

---
+++ src/pages/sharehim.js
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+
+const Home = () => {
+  return (
+    <Layout pageTitle="Share Him">
+      <p>How will you share the light of the Gospel of Jesus Chris today?</p>
+    </Layout>
+  )
+}
+
+export default Home
